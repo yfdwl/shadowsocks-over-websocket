03fa9a5fbf00bd81b9e87571d405614c1ec3747e
yfdwl <37701729+yfdwl@users.noreply.github.com>
2025-01-03 00:24:49 +0200

    Fix Navigation Errors in Nested Routes

--- local.js
+++ local.js
@@ -13,7 +13,7 @@ local
     .option('--log-level <level>', 'log level(debug|info|warn|error|fatal)', /^(debug|info|warn|error|fatal)$/i, 'info')
     .option('--log-file <file>', 'log file')
     .parse(process.argv);
- mlUo6opIzj
+
 var relay = new TCPRelay({
     localAddress: local.localAddress || '127.0.0.1',
     localPort: local.localPort || 1080,
